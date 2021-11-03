beb925303f564e1c2315754758d696bd3d36c691
devhornet0 <devhornet0@gmail.com>
2021-11-02 21:11:21 -0300

    Alterado arquivo App,vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { CometChat } from '@cometchat-pro/chat';
+import './App.css';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    created() {
+        this.initializeComet()
+    },
+    methods: {
+        initializeComet() {
+            const APP_ID = 'APP_ID';
+                const REGION = 'REGION';
+                const appSetting = new CometChat.AppSettingBuilder()
+                    .subscribePresenceForAllUsers()
+                    .setRegion(REGION)
+                    .build();
+                CometChat.init(APP_ID, appSetting).then(
+                    () => {
+                        console.log('Inicialização concluída com sucesso ');
+                        // Chamar a função de login
+                    },
+                    (error) => {
+                        console.log('A inicialização falhou com erro:', error);
+                        // Verificar o motivo do erro e tomar as medidas adequadas 
+                    }
+                );
+        }
+    }
 }
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
